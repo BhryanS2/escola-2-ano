833428a05dd36a3c8862c35671a108205cdcd5b5
BhryanS2 <bhryanstepenhen@gmail.com>
2021-12-16 10:52:36 -0300

    arrumando para gravaçao

--- linquagem de programação/4º Bimeste/Semana 2/CRUD/src/view/index.java
+++ linquagem de programação/4º Bimeste/Semana 2/CRUD/src/view/index.java
@@ -1,15 +1,12 @@
-
 package view;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class index {
-  private Map<Integer, String> data = new HashMap<Integer, String>();
   private String[] columnNames = { "Nome do Produto", "Descrição do Produto", "Data de Cadastro", "Quantidade",
       "Preço" };
   JFrame frame;
@@ -38,6 +35,7 @@ public class index {
   private JLabel lblRegister = new JLabel("Data de Cadastro");
   private JLabel lblAmount = new JLabel("Quantidade");
   private JLabel lblValue = new JLabel("Preço");
+  private JLabel lblTitle = new JLabel("Cadastro de Produtos | Supermercado");
   private Color white = new Color(255, 255, 255);
   private Color background = new Color(242, 243, 244);
   private Color text = new Color(102, 102, 102);
@@ -46,18 +44,6 @@ public class index {
   private Color green = new Color(76, 214, 43);
   private Color blue = new Color(89, 101, 224);
   private Color blueDark = new Color(73, 83, 184);
-
-  /**
-   * Launch the application.
-   */
-
-  public void setData(Map<Integer, String> data) {
-    this.data = data;
-  }
-
-  public Map<Integer, String> getData() {
-    return data;
-  }
 
   public static void main(String[] args) {
     EventQueue.invokeLater(new Runnable() {
@@ -73,17 +59,12 @@ public class index {
     });
   }
 
-  /**
-   * Create the application.
-   */
   public index() {
     initialize();
   }
 
-  /**
-   * Initialize the contents of the frame.
-   */
   private void definePositionComponents() {
+    lblTitle.setBounds(WIDTH - 5, POSITION - 15, widthElement + 20, HEIGHT);
 
     btnRegister = new JButton("Cadastrar");
     btnUpdate = new JButton("Atualizar");
@@ -99,24 +80,24 @@ public class index {
     frame.setBounds(100, 100, width, height);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    lblName.setBounds(WIDTH, POSITION, widthElement, HEIGHT);
-    txtName.setBounds(WIDTH, POSITION * 2, widthElement, HEIGHT);
+    lblName.setBounds(WIDTH, POSITION * 2, widthElement, HEIGHT);
+    txtName.setBounds(WIDTH, POSITION * 3, widthElement, HEIGHT);
 
-    lblRegister.setBounds(WIDTH, POSITION * 3, widthElement, HEIGHT);
-    txtRegister.setBounds(WIDTH, POSITION * 4, widthElement, HEIGHT);
+    lblRegister.setBounds(WIDTH, POSITION * 4, widthElement, HEIGHT);
+    txtRegister.setBounds(WIDTH, POSITION * 5, widthElement, HEIGHT);
 
-    lblDescription.setBounds(WIDTH, POSITION * 5, widthElement, HEIGHT);
-    txtDescription.setBounds(WIDTH, POSITION * 6, widthElement, HEIGHT);
+    lblDescription.setBounds(WIDTH, POSITION * 6, widthElement, HEIGHT);
+    txtDescription.setBounds(WIDTH, POSITION * 7, widthElement, HEIGHT);
 
-    lblAmount.setBounds(WIDTH, POSITION * 7, widthElement, HEIGHT);
-    txtAmount.setBounds(WIDTH, POSITION * 8, widthElement, HEIGHT);
+    lblAmount.setBounds(WIDTH, POSITION * 8, widthElement, HEIGHT);
+    txtAmount.setBounds(WIDTH, POSITION * 9, widthElement, HEIGHT);
 
-    lblValue.setBounds(WIDTH, POSITION * 9, widthElement, HEIGHT);
-    txtValue.setBounds(WIDTH, POSITION * 10, widthElement, HEIGHT);
+    lblValue.setBounds(WIDTH, POSITION * 10, widthElement, HEIGHT);
+    txtValue.setBounds(WIDTH, POSITION * 11, widthElement, HEIGHT);
 
-    btnRegister.setBounds(BUTTON_WIDTH_Center, POSITION * 11, BUTTON_WIDTH, HEIGHT);
-    btnUpdate.setBounds(ESPACE_BUTTON, POSITION * 11, BUTTON_WIDTH, HEIGHT);
-    btnDelete.setBounds(ESPACE_BUTTON + BUTTON_WIDTH + 50, POSITION * 11, BUTTON_WIDTH, HEIGHT);
+    btnRegister.setBounds(BUTTON_WIDTH_Center, POSITION * 12, BUTTON_WIDTH, HEIGHT);
+    btnUpdate.setBounds(ESPACE_BUTTON, POSITION * 12, BUTTON_WIDTH, HEIGHT);
+    btnDelete.setBounds(ESPACE_BUTTON + BUTTON_WIDTH + 50, POSITION * 12, BUTTON_WIDTH, HEIGHT);
 
     txtName.setColumns(COLUMNS);
     txtRegister.setColumns(COLUMNS);
@@ -129,6 +110,7 @@ public class index {
     // colors and fonts
     frame.getContentPane().setBackground(background);
     lblName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+    lblTitle.setFont(new Font("Arial", Font.BOLD, 20));
     lblDescription.setFont(new Font("Tahoma", Font.PLAIN, 14));
     lblRegister.setFont(new Font("Tahoma", Font.PLAIN, 14));
     lblAmount.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -143,6 +125,7 @@ public class index {
     btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 14));
 
     // colors
+    lblTitle.setForeground(text);
     lblName.setForeground(text);
     lblDescription.setForeground(text);
     lblRegister.setForeground(text);
@@ -314,7 +297,8 @@ public class index {
     defineStyleComponents();
     Container contentFrame = frame.getContentPane();
     contentFrame.setLayout(null);
-    Object[] components = { lblName, txtName, lblRegister, txtRegister, lblDescription, txtDescription, lblAmount,
+    Object[] components = { lblTitle, lblName, txtName, lblRegister, txtRegister, lblDescription, txtDescription,
+        lblAmount,
         txtAmount, lblValue, txtValue, btnRegister, btnUpdate, btnDelete };
     for (Object component : components) {
       contentFrame.add((Component) component);
@@ -354,8 +338,8 @@ public class index {
       }
       result = this.resgister();
       result = result.length() > 0 ? result : "Erro ao registrar";
-      showTable();
       JOptionPane.showMessageDialog(null, result);
+      clearFields();
     });
 
     btnUpdate.addActionListener(e -> {
@@ -367,24 +351,25 @@ public class index {
         return;
       }
       result = this.update();
-      result = result.length() > 0 ? result : "Erro ao fazer update";
-      showTable();
       JOptionPane.showMessageDialog(null, result);
+      clearFields();
     });
 
     btnDelete.addActionListener(e -> {
       String result = "";
-      boolean isValidFields = true;
-      isValidFields = ValidateFields();
-      if (!isValidFields) {
-        JOptionPane.showMessageDialog(null, "Preencha todos os campos");
-        return;
-      }
       result = this.Delete();
       result = result.length() > 0 ? result : "Erro ao deletar";
-      showTable();
       JOptionPane.showMessageDialog(null, result);
+      clearFields();
     });
+  }
+
+  private void clearFields() {
+    txtName.setText("");
+    txtRegister.setText("");
+    txtDescription.setText("");
+    txtAmount.setText("");
+    txtValue.setText("");
   }
 
   private boolean ValidateFields() {
@@ -398,25 +383,6 @@ public class index {
     return true;
   }
 
-  private void showTable() {
-    DefaultTableModel model = (DefaultTableModel) table.getModel();
-    model.setDataVector(new Object[][] {}, columnNames);
-    table.setModel(model);
-    // transform data in Object[][]
-    Object[][] objData = new Object[data.size()][columnNames.length];
-    for (int i = 0; i < data.size(); i++) {
-      String values = data.get(i);
-      if (values != null) {
-        String[] valuesSplit = values.split(";");
-        for (int j = 0; j < valuesSplit.length; j++) {
-          objData[i][j] = valuesSplit[j];
-        }
-      }
-    }
-    model.setDataVector(objData, columnNames);
-    table.setModel(model);
-  }
-
   private Object[] products() {
     String name = txtName.getText();
     String register = txtRegister.getText();
@@ -427,115 +393,41 @@ public class index {
     return product;
   };
 
-  private Boolean indexOf(Object[] product) {
-    Boolean res = false;
-    for (int i = 0; i < data.size(); i++) {
-      if (data.containsKey(i)) {
-        String[] values = data.get(i).split(";");
-        String name = values[0];
-        if (name.equals(product[0])) {
-          res = true;
-          return res;
-        }
-      }
-    }
-    return res;
-  }
-
-  private int findProduct(String register) {
-    int index = 0;
-    int left = 0;
-    int right = data.size() - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      String[] values = data.get(mid).split(";");
-      String name = values[0];
-      if (name.equals(register)) {
-        index = mid;
-        break;
-      } else if (name.compareTo(register) < 0) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    return index;
-  }
-
   private String resgister() {
     String result = "";
-    Object[] product = products();
-    Boolean res = indexOf(product);
-    if (res) {
-      result = "Produto já cadastrado";
-      return result;
-    }
-    // add product in matriz string
-    String productString = "";
-    for (int i = 0; i < product.length; i++) {
-      productString += product[i] + ";";
-    }
-    int row = table.getSelectedRow();
-    if (row == -1) {
-      data.put(data.size(), productString);
-    } else {
-      data.put(row, productString);
-    }
-    result = "Produto cadastrado com sucesso";
+    DefaultTableModel model = (DefaultTableModel) table.getModel();
+    Object[] data = products();
+    model.addRow(data);
+    result = "Produto Cadastrado com sucesso";
     return result;
   }
 
   private String update() {
     String result = "";
-    Object[] product = products();
-    String newValue = product[0] + ";" + product[1] + ";" + product[2] + ";" + product[3] + ";" + product[4];
-    String nameNew = product[0].toString();
+    Object[] data = products();
     int row = table.getSelectedRow();
-
-    if (row != -1) {
-      String oldValue = data.get(row);
-      String nameOld = oldValue.split(";")[0];
-      if (!nameOld.equals(nameNew)) {
-        result = "Não é possível alterar o nome do produto";
-        return result;
-      }
-      data.put(row, newValue);
-      return result;
-    }
-    // Not line selected
-
-    Boolean exists = indexOf(product);
-    if (!exists) {
-      result = "Produto não cadastrado";
+    if (row == -1) {
+      result = "Selecione uma linha";
       return result;
     }
 
-    int index = findProduct(nameNew);
-    data.replace(index, newValue);
-    result = "Produto atualizado com sucesso";
+    for (int i = 0; i < data.length; i++) {
+      table.setValueAt(data[i], row, i);
+    }
+    result = "Produto autulizado com sucesso";
     return result;
   }
 
   private String Delete() {
     String result = "";
-    Object[] product = products();
-    Boolean exists = indexOf(product);
-    if (!exists) {
-      result = "Produto não cadastrado";
+    int row = table.getSelectedRow();
+    if (row == -1) {
+      result = "Erro ao deletar, Selecione uma linha";
       return result;
     }
-    int row = table.getSelectedRow();
-    if (row != -1) {
-      data.remove(row);
-      result = "Produto deletado com sucesso";
-    } else {
-      // Not line selected
-      String name = product[0].toString();
-      int index = findProduct(name);
-      data.remove(index);
-      result = "Produto removido com sucesso";
-    }
-
+    DefaultTableModel model = (DefaultTableModel) table.getModel();
+    model.removeRow(row);
+    result = "Linha removida com sucesso";
     return result;
   }
 }
--- linquagem de programação/4º Bimeste/Semana 2/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author bhryan
- *
- */
-module view {
-}
